fba9b46e5700cdc48e829d0a2d5ce19d4c7ae4ac
Milena Suzumura <milena.suzumura11@gmail.com>
2024-01-27 18:07:35 -0300

    feat: criei os arquivos para a rota planets

--- app/backend/src/routes/planets.routes.js
+++ app/backend/src/routes/planets.routes.js
@@ -3,10 +3,6 @@ const { Planet } = require('../models');
 
 const planetsRouter = Router();
 
-planetsRouter.get('/', async (_req, res) => {
-    const test = await Planet.findAll({ raw: true });
-    console.log(test);
-    return res.status(200).json(test);
-});
+planetsRouter.get('/', );
 
 module.exports = planetsRouter;
